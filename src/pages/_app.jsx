8463de62e00fabc46fb558cd1c078fbe7d30cb84
koheifairman <osigoto0817@gmail.com>
2024-10-08 17:38:03 +0900

    add Google Tag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,43 @@
 import "../styles/global.css";
 import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const GA_TRACKING_ID = "G-K1GH021HLQ"; // googleアナリティクスのトラッキングID
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Head>
+        {/* Google Tag (gtag.js) */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${GA_TRACKING_ID}');
+                        `,
+          }}
+        />
         <title>むちむち無知の知くんのサイト</title>
       </Head>
       <Component {...pageProps} />
